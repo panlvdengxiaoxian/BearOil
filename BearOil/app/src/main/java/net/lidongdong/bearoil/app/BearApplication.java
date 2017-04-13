8e56e07b21e01c57157b4618464636a71928562f
dllo <dllo@dllodeMac-mini-119.local>
2017-04-13 17:22:02 +0800

    在 Application中设置 context

---
+++ BearOil/app/src/main/java/net/lidongdong/bearoil/app/BearApplication.java
@@ -0,0 +1,29 @@
+package net.lidongdong.bearoil.app;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ *
+ *
+ *  @author lidongdong(一个帅的惊天动地的男人)
+ *  @date 17/4/13
+ *  @explain
+ *  @function
+ *  @version 1.0
+ *
+ */
+
+public class BearApplication extends Application {
+    private static Context sContext;
+
+    @Override
+    public void onCreate () {
+        super.onCreate();
+        sContext = this;
+    }
+
+    public static Context getContext () {
+        return sContext;
+    }
+}
